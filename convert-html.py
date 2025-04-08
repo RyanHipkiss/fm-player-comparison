fb3961155d02a4798f5a2229ab356dddc5f091f5
Ryan Hipkiss <ryan@ryanhipkiss.co.uk>
2025-04-08 06:07:40 +0100

    Ability for AF(A) along with displaying some stats

--- convert-html.py
+++ convert-html.py
@@ -1,6 +1,6 @@
 from classes.Convert import Convert
 
 print("Converting")
-convert = Convert('example-data/players.html', 'example-data/players.csv')
+convert = Convert('example-data/wolves-moneyball-example-player-list.html', 'example-data/wolves-moneyball-example-player-list.csv')
 convert.convert()
 print("Finished converting")
